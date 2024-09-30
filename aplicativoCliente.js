d55c9a386d666f2d3ddba7c66cf06e914e124a87
Andrés Erazo <erazoandres14@gmail.com>
2024-09-29 21:35:15 -0500

    t

--- aplicativoCliente.js
+++ aplicativoCliente.js
@@ -299,6 +299,8 @@ closeModalButton.addEventListener('click', function(event) {
     window.modalCerradoPorBoton = true;
     const pedidoModal = bootstrap.Modal.getInstance(document.getElementById('pedidoModal'));
     pedidoModal.hide(); // Cerrar el modal
+
+    
   }
 });
 
